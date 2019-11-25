a1da40642d12480211350ef33fe7d855ebac4b33
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-24 20:27:13 -0500

    chore: Initial express setup

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
+/* Request project dependencies*/
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +17,16 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
-// Setup Server
+// Start server 
+const PORT = 3000;
+const serverCallback = () => {
+    console.log(`Server started. Listening on port ${PORT}`);
+}
+
+app.listen(PORT, serverCallback);
