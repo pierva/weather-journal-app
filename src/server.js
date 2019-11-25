272528afac347f2a87585c32307ee8b4ebaa51d1
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-24 20:46:30 -0500

    refactor: Change folder structure

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
+app.use(express.static('static/website'));
 
 
 // Start server 
