08a69e1812dd5c5890669e17ce1c2614f433eed8
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-26 23:22:27 -0500

    fix: Add body parser to router instead of app

--- src/server.js
+++ src/server.js
@@ -1,14 +1,9 @@
 /* Request project dependencies*/
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 
 // Import routes
 const routes = require('./routes');
-
-
-// Setup empty JS object to act as endpoint for all routes
-projectData = {};
 
 // Create an app instance
 const app = express();
@@ -16,13 +11,9 @@ const app = express();
 // Connect all the routes to the application 
 app.use('/', routes);
 
-
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
-// Cors for cross origin allowance
 app.use(cors());
 
 // Initialize the main project folder
