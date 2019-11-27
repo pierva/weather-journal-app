e0b1773a36c147ef6cd4cc8b56a23f0f284a1107
piervalerio.vignola@gmail.com <piervalerio.vignola@gmail.com>
2019-11-26 23:22:03 -0500

    feat: Post data to server from client

--- public/website/app.js
+++ public/website/app.js
@@ -3,7 +3,9 @@
 /* Global Variables */
 const model = {
     inputSelectors: ['#zip', '#feelings'],
+    origin: window.location.origin,
     weatherPath: '/weather',
+    postPath: '/postdata'
 };
 
 const octopus = {
@@ -13,23 +15,48 @@ const octopus = {
 
     getWeatherPath: () => model.weatherPath,
 
+    getPostPath: () => model.postPath,
+
     generateNewDate: () => {
         // Create a new date instance dynamically with JS
         let d = new Date();
-        return d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+        return d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
     },
 
     getInputSelectors: () => {
         return model.inputSelectors;
     },
 
-    getWeatherData: async (url = '') => {
-        const response = await fetch(url);
+    getWeatherData: async (path = '') => {
+        const response = await fetch(model.origin + path);
         try {
             const data = await response.json();
-            console.log(data); 
+            console.log(data);
+            return data;
         } catch (error) {
             console.log('error: ', error);
+            return error;
+        }
+    },
+
+    postData: async (path = '', data = {}) => {
+        // response returns a promise
+        const response = await fetch(model.origin + path, {
+            method: 'POST',
+            credentials: 'same-origin',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data),
+        });
+
+        try {
+            const newData = await response.json();
+            console.log(newData);
+            return newData;
+        } catch (error) {
+            return error;
         }
     }
 }
@@ -38,7 +65,7 @@ const view = {
     // Arrow function 
     init: () => {
         // Wait until the DOM is fully loaded before adding event listeners
-        document.addEventListener('DOMContentLoaded', ()=> {
+        document.addEventListener('DOMContentLoaded', () => {
             view.addEventListeners();
         })
     },
@@ -57,17 +84,18 @@ const view = {
     addEventListeners: () => {
         const submitButton = document.getElementById('generate');
         submitButton.addEventListener('click', (event) => {
-            console.log('clicked')
             // Prevent default is needed in case we use a form, this 
             // way we prevent page reload
             event.preventDefault();
             // get user inputs
             const selectors = octopus.getInputSelectors();
             const inputs = view.getUserInputs(selectors);
-            const origin = window.location.origin;
-            octopus.getWeatherData(origin + octopus.getWeatherPath())
             
-
+            octopus.getWeatherData(
+                octopus.getWeatherPath() + '?zip=' + inputs.zip)
+                .then((data) => {
+                    octopus.postData(octopus.getPostPath(), inputs)
+                })
         })
     }
 }
